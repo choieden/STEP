df164ab72da73877388f7dd3bc3e60c68bca12e7
oungjun choi y <youngjunchoi@youngjunui-MacBook-Air.local>
2016-12-11 16:01:26 +0900

    Initial Commit

---
+++ STEP/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  STEP
+//
+//  Created by youngjun choi on 2016. 12. 11..
+//  Copyright © 2016년 youngjun choi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ STEP/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  STEP
+//
+//  Created by youngjun choi on 2016. 12. 11..
+//  Copyright © 2016년 youngjun choi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
